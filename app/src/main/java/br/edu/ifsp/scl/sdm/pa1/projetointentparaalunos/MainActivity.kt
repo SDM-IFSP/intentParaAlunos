7bff3a5c9753d1697e9e9315efc208c2e42cca3e
paula <paula.millanii@gmail.com>
2021-09-17 22:01:46 -0300

    first commit

--- app/src/main/java/br/edu/ifsp/scl/sdm/pa1/projetointentparaalunos/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/pa1/projetointentparaalunos/MainActivity.kt
@@ -1,14 +1,23 @@
 package br.edu.ifsp.scl.sdm.pa1.projetointentparaalunos
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.sdm.pa1.projetointentparaalunos.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var requisicaoPermissoesActivityResultLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +30,16 @@ class MainActivity : AppCompatActivity() {
 
         // Usando Toolbar como ActionBar da Activity
         setSupportActionBar(activityMainBinding.mainTb.appTb)
+
+        requisicaoPermissoesActivityResultLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { concedida ->
+            if( !concedida ) {
+                requisitarPermissaoLigacao()
+
+            }
+            else {
+                discarTelefone()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -31,12 +50,31 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.viewMi -> {
+                val url = activityMainBinding.parameterEt.text.toString().let {
+                    if (!it.contains("http[s]?".toRegex())) "http://${it}" else it
+                }
+                val siteIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(siteIntent)
                 true
             }
             R.id.callMi -> {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if(checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        //requisitar permissao
+                    } else{
+                        //executar a discagem
+                        discarTelefone()
+                    }
+                } else {
+                    //executar discagem
+                    discarTelefone()
+                }
                 true
             }
             R.id.dialMi -> {
+                val ligacaoIntent = Intent(Intent.ACTION_DIAL)
+                ligacaoIntent.data = Uri.parse("${activityMainBinding.parameterEt.text}")
+                startActivity(ligacaoIntent)
                 true
             }
             R.id.pickMi -> {
@@ -63,4 +101,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun discarTelefone() {
+        val discarIntent = Intent()
+        discarIntent.action = Intent.ACTION_CALL
+        discarIntent.data = Uri.parse("tel: ${activityMainBinding.parameterEt.text}")
+        startActivity(discarIntent)
+    }
+
+    private fun requisitarPermissaoLigacao() = requisicaoPermissoesActivityResultLauncher.launch((Manifest.permission.CALL_PHONE))
 }
